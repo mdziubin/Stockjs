16cae4be60a50b4f414755526e1b3eb168234018
mdziubin <mdziubin@ualberta.ca>
2020-08-17 17:20:48 -0600

    Added CORS middleware

--- app.js
+++ app.js
@@ -12,6 +12,15 @@ const app = express();
 
 // Middleware
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 app.use("/test", testRoutes);
 app.use("/auth", authRoutes);
 
